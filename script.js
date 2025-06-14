e1eb0bfa094f974f40745a7452915f76cfc5c5a1
abhi <atheleteswar@gmail.com>
2025-06-15 00:41:50 +0530

    Changed background as wel as added add book mark option

--- script.js
+++ script.js
@@ -1,2 +1,51 @@
-/alert("this is bookmark created bye me")
+alert("this is bookmark created bye me")
+document.getElementById('bookmark-form').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e) {
+  e.preventDefault();
+
+  const name = document.getElementById('siteName').value;
+  const url = document.getElementById('siteURL').value;
+
+  if (!name || !url) {
+    alert("Please fill out both fields!");
+    return;
+  }
+
+  const bookmark = {
+    name,
+    url
+  };
+
+  let bookmarks = localStorage.getItem('bookmarks')
+    ? JSON.parse(localStorage.getItem('bookmarks'))
+    : [];
+
+  bookmarks.push(bookmark);
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  document.getElementById('bookmark-form').reset();
+  showBookmarks();
+}
+
+function showBookmarks() {
+  const bookmarks = JSON.parse(localStorage.getItem('bookmarks')) || [];
+  const bookmarksList = document.getElementById('bookmarksList');
+
+  bookmarksList.innerHTML = '';
+
+  bookmarks.forEach((bookmark, index) => {
+    bookmarksList.innerHTML += `
+      <div>
+        <h3>${bookmark.name}</h3>
+        <a href="${bookmark.url}" target="_blank">Visit</a>
+      </div>
+      <hr>
+    `;
+  });
+}
+
+// Load bookmarks on page load
+document.addEventListener('DOMContentLoaded', showBookmarks);
+
 
